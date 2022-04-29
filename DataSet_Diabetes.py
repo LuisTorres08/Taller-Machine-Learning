581224d4923b125a113b0ab96a019398cc87be2d
Luis Torres <luisdeavila010@gmail.com>
2022-04-28 22:36:39 -0500

    Tratamiento de la data

--- DataSet_Diabetes.py
+++ DataSet_Diabetes.py
@@ -14,3 +14,15 @@ url = "diabetes.csv"
 data = pd.read_csv(url)
 
 
+# Tratamiento de la data (Limpiar y normalizar la data)
+
+data.drop(['Pregnancies', 'BloodPressure', 'SkinThinckness'], axis=1, inplace=True)
+
+
+ranges = [0, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+ranges_names = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+data.Age = pd.cut(data.Age, ranges, labels=ranges_names)
+data.Age.value_counts()
+
+data.dropna(axis=0, how='any', inplace=True)
+
