ea400383411cd26e547faa64bfc81ae35955a845
Luis Torres <luisdeavila010@gmail.com>
2022-04-28 22:22:50 -0500

    Obtención de la data

---
+++ DataSet_Diabetes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 22:03:44 2022
+
+@author: Luis Torres
+"""
+
+import numpy as np
+import pandas as pd
+
+# Obtener la data
+
+url = "diabetes.csv"
+data = pd.read_csv(url)
+
+
