12ffc4d783bcb408362e491f53d962b07576eea3
Luis Torres <luisdeavila010@gmail.com>
2022-04-28 23:17:13 -0500

    Tratamiento de la data

--- DataSet_WeatherAUS.py
+++ DataSet_WeatherAUS.py
@@ -14,5 +14,17 @@ import pandas as pd
 url = "weatherAUS.csv"
 data = pd.read_csv(url)[:3000]
 
+# Tratamiento de la data (Limpiar y normalizar la data)
+
+data.RainToday.replace(['yes', 'no'], [1, 0], inplace=True)
+data.RainToday.value_counts()
+
+data.RainTomorrow.replace(['no', 'yes'], [0, 1], inplace=True)
+data.RainTomorrow.value_counts()
+
+data.drop(['Date', 'Location', 'Evaporation', 'Sunshine', 'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm', 'WindSpeed9am', 'WindSpeed3pm'], axis=1, inplace=True)
+
+data.dropna(axis=0, how='any', inplace=True)
+
 
 
