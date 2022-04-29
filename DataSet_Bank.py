2a97f719c7e7010da48ed3e58bd1a47df1d19c47
Luis Torres <luisdeavila010@gmail.com>
2022-04-28 21:58:39 -0500

    Seleccion del modelo, entrenamiento del modelo y sección de metricas

--- DataSet_Bank.py
+++ DataSet_Bank.py
@@ -9,6 +9,12 @@ Created on Thu Apr 28 21:05:49 2022
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 # Obtener la data
 
@@ -68,6 +74,145 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 x_test_out = np.array(data_test.drop(['y'], axis=1))
 y_test_out = np.array(data_test.y)
 
+
+# Regresion logistica
+
+# Seleccionar un modelo
+
+logreg = LogisticRegression(solver='lbfgs', max_iter=7600)
+
+# Entrenar el modelo
+
+logreg.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('Regresion Logistica')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
+
+# Accuracy de entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test_out, y_test_out)}')
+
+
+# Maquina de soporte vectorial
+
+# Seleccionar un modelo
+
+svc = SVC(gamma='auto')
+
+# Entrenar el modelo
+
+svc.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('Maquina de soporte vectorial')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
+
+# Accuracy de entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {svc.score(x_test_out, y_test_out)}')
+
+
+# Arbol de decisión
+
+# Seleccionar un modelo
+
+tree = DecisionTreeClassifier()
+
+# Entrenar el modelo
+
+tree.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('Arbol de decisión')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {tree.score(x_test, y_test)}')
+
+# Accuracy de entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {tree.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {tree.score(x_test_out, y_test_out)}')
+
+
+# K-Nearest neighbors
+
+# Seleccionar un modelo
+
+kneighbors = KNeighborsClassifier()
+
+# Entrenar el modelo
+
+kneighbors.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('K-Nearest neighbors')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {kneighbors.score(x_test, y_test)}')
+
+# Accuracy de entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {kneighbors.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {kneighbors.score(x_test_out, y_test_out)}')
+
+
+# Random Forest
+
+# Seleccionar un modelo
+
+random_forest = RandomForestClassifier()
+
+# Entrenar el modelo
+
+random_forest.fit(x_train, y_train)
+
+# Metricas
+
+print('*'*50)
+print('Random Forest')
+
+# Accuracy de Test de Entrenamiento
+
+print(f'accuracy de Test de Entrenamiento: {random_forest.score(x_test, y_test)}')
+
+# Accuracy de Entrenamiento de Entrenamiento
+
+print(f'accuracy de Entrenamiento de Entrenamiento: {random_forest.score(x_train, y_train)}')
+
+# Accuracy de validacion
+
+print(f'accuracy de Test de Entrenamiento: {random_forest.score(x_test_out, y_test_out)}')
 
 
 
