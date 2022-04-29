f209bfc719f09ab2501d129c521626c3f3eb24b6
Luis Torres <luisdeavila010@gmail.com>
2022-04-28 23:23:42 -0500

    Data dividida en train y test

--- DataSet_WeatherAUS.py
+++ DataSet_WeatherAUS.py
@@ -8,6 +8,7 @@ Created on Thu Apr 28 22:51:31 2022
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Obtener la data
 
@@ -26,5 +27,17 @@ data.drop(['Date', 'Location', 'Evaporation', 'Sunshine', 'WindGustDir', 'WindGu
 
 data.dropna(axis=0, how='any', inplace=True)
 
+
+# Partir la data en dos
+data_train = data[:471]
+data_test = data[471:]
+
+x = np.array(data_train.drop(['RainTomorrow'], axis=1))
+y = np.array(data_test.RainTomorrow)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+x_test_out = np.array(data_test.drop(['RainTomorrow'], axis=1))
+y_test_out = np.array(data_test.RainTomorrow)
 
 
